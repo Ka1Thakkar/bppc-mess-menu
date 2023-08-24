cf377c9eb0b28bf96792f5a0b58da25b887afbbf
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-08-24 12:25:54 +0530

    navbar issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" className=''>
+    <html lang="en">
       <Nav />
       <body className={inter.className}>{children}</body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -36,7 +36,7 @@ const Nav = () => {
         },
     ]
     return (
-        <nav className="absolute bottom-0 lg:left-0 lg:top-0 flex lg:flex-col justify-around lg:h-screen w-screen lg:w-auto items-center bg-textgreen rounded-t-lg lg:rounded-tl-none lg:rounded-r-lg border-t-4 lg:border-t-0 border-black">
+        <nav className="absolute bottom-0 z-40 lg:left-0 lg:top-0 flex lg:flex-col justify-around lg:h-screen w-screen lg:w-auto items-center bg-textgreen rounded-t-lg lg:rounded-tl-none lg:rounded-r-lg border-t-4 lg:border-t-0 border-black">
             {days.map((day, key)=>(
                 <Link href={day.link} className="h-full w-full">
                 <button className="text-black py-3 px-5 font-bold text-xl hover:bg-black hover:text-textgreen w-full h-full transition-all ease-in-out">
