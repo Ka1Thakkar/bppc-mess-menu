38696d8d36456fea75ace95bcc28ab0f2b5accdb
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-08-25 08:31:19 +0530

    Moved the ssms logo upwards on the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -26,10 +26,10 @@ export default function Home() {
       <Image src={background} style={{objectFit: 'cover'}} quality={100} alt='background' className='absolute w-screen min-h-screen md:h-screen bg-bggray' />
       <div className=' absolute min-h-screen w-screen bg-gradient-to-b from-transparent via-black/90 to-black'></div>
       <div className='bg-black bottom-0 absolute h-screen'></div>
-      <div className='absolute right-5 top-10'>
+      <div className='absolute right-5 top-5 lg:top-10'>
         <SSMSLogo />
       </div>
-      <section className='absolute w-screen min-h-screen px-10 lg:px-20 xl:px-40 flex flex-col items-center justify-center pt-12 lg:pt-0'>
+      <section className='absolute w-screen min-h-screen px-10 lg:px-20 xl:px-40 flex flex-col items-center justify-center pt-10 lg:pt-0'>
       <h1 className={pacifico.className + " text-6xl md:text-7xl text-center lg:text-8xl text-white font-black pt-10"}>Today's Mess Menu</h1>
       <div className='pb-10'>
         <h1 className='text-3xl font-medium text-gray-400 pt-2'>{date}</h1>
