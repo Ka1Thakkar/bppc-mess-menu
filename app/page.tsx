bd539b0ac38525125d8ac44550de3c313274933d
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-09-03 11:28:00 +0530

    updated main page and menu for this week

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
 'use client'
 import Image from 'next/image'
 import { Lilita_One, Montserrat, Pacifico } from 'next/font/google'
-import { IBM_Plex_Sans } from 'next/font/google'
 import background from '@/public/assets/Pattern.svg'
-import data from '@/data/menu'
+import data from '@/data/timings'
 import SSMSLogo from '@/components/SSMSLogo'
 import BottomBar from '@/components/BottomBar'
-import { GetServerSideProps } from 'next'
-import { useEffect, useState } from 'react'
 
 const pacifico = Lilita_One({weight:['400'], subsets:['latin']})
 
@@ -28,51 +25,6 @@ export default function Home() {
   ]
 
   var date =  new Date().toUTCString().slice(5, 16)
-  
-  // var date = ""
-
-  // function setDate() {
-  //   date =  new Date().toUTCString().slice(5, 16)
-  //   return date
-  // }
-
-  // var selected_day = 0
-
-  // function setMenuDay() {
-  //   selected_day = (new Date().getDay() + 6) % 7
-  //   return selected_day
-  // }
-
-  // window.onbeforeunload = setDate;
-  // window.onbeforeunload = setMenuDay;
-
-  // addEventListener("load", (event) => {
-  //   var date = new Date().toUTCString().slice(5, 16)
-  //   var selected_day = (new Date().getDay() + 6) % 7
-  // })
-
-  // var date = addEventListener("load", (event) => {
-  //   return new Date().toUTCString().slice(5, 16)
-  // })
-
-  // var selected_day = addEventListener("load", (event) => {
-  //   return (new Date().getDay() + 6) % 7
-  // })
-
-  // var date = new Date().toUTCString().slice(5, 16);
-
-  // let selected_day = 0
-  // selected_day = (new Date().getDay() + 6) % 7
-  // let date = new Date().toUTCString().slice(5, 16);
-
-  // let d = Date(Date.now());
- 
-  // // Converting the number of millisecond
-  // // in date string
-  // const a = d.toString()
- 
-  // // Printing the current date
-  // console.log("The current date is: " + a)
 
   return (
     <main className=' bg-black min-h-screen relative overflow-x-hidden'>
@@ -83,31 +35,31 @@ export default function Home() {
         <SSMSLogo />
       </div>
       <section className='absolute w-screen min-h-screen px-10 lg:px-20 xl:px-40 flex flex-col items-center justify-center pt-10 lg:pt-0 pb-28 lg:pb-0'>
-      <h1 className={pacifico.className + " text-6xl md:text-7xl text-center lg:text-8xl text-white font-black pt-10"}>Today's Mess Menu</h1>
+      <h1 className={pacifico.className + " text-6xl md:text-7xl text-center lg:text-8xl text-white font-black pt-10"}>Mess Timings</h1>
       <div className='pb-10'>
-        <h1 className='text-3xl font-medium text-gray-400 pt-2'>{data[selected_day].Date}</h1>
+        <h1 className='text-3xl font-medium text-gray-400 pt-2'></h1>
       </div>
       <div className={mont.className + ' flex flex-col lg:grid lg:grid-cols-3 w-full gap-10 text-black pb-20'}>
         <div className=' flex flex-col justify-start items-center p-10 border-4 rounded-xl text-white border-textgreen/50'>
         <h1 className='lg:text-5xl text-4xl pb-5 font-extrabold text-textgreen'>Breakfast</h1>
-          <div className=''>
+          {/* <div className=''>
             {breakfast_consts.map((breakfast, i)=>(
               <h1 key={i} className='text-center text-base lg:text-xl'>{breakfast}</h1>
             ))}
-          </div>
-          <div className='flex flex-col gap-2 text-base lg:text-xl font-medium text-center'>{data[selected_day].B.map((menuitem, key) => (
+          </div> */}
+          <div className='flex flex-col gap-2 text-base lg:text-xl font-medium text-center'>{data[0].B.map((menuitem, key) => (
             <h1 key={key}>{menuitem}</h1>
           ))}</div>
         </div>
         <div className=' flex flex-col justify-start items-center p-10 border-4 border-textgreen/50 text-white rounded-xl'>
           <h1 className='text-4xl lg:text-5xl pb-5 font-extrabold text-textgreen'>Lunch</h1>
-          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center'>{data[selected_day].L.map((menuitem, key) => (
+          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center'>{data[0].L.map((menuitem, key) => (
             <h1 key={key}>{menuitem}</h1>
           ))}</div>
         </div>
         <div className=' flex flex-col justify-start items-center p-10 border-4 border-textgreen/50 text-white rounded-xl'>
           <h1 className='text-4xl md:text-5xl pb-5 font-extrabold text-textgreen'>Dinner</h1>
-          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center'>{data[selected_day].D.map((menuitem, key) => (
+          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center'>{data[0].D.map((menuitem, key) => (
             <h1 key={key}>{menuitem}</h1>
           ))}</div>
         </div>
---
+++ data/timings.js
@@ -0,0 +1,8 @@
+const data = [{
+    B: ["07:30 AM - 09:30 AM", "08:00 AM - 10:00 AM (Sundays and Holidays)"],
+    L: ["11:30 AM - 01:30 PM", "12:00 PM - 02:00 PM (Sundays and Holidays)"],
+    D: ["07:00 PM - 08:45 PM", "(08:45PM Last Entry Time)"]
+},
+]
+
+export default data;
