5fe63e8cbb33a2d5468178f2b17e3aaacbe26427
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-10-01 08:06:48 +0530

    Added the menu download functionality

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -5,7 +5,7 @@ const BottomBar = () => {
         <main className="absolute bottom-20 lg:bottom-5 w-full">
             <h1 className="text-base text-white/60 text-center">Developed and maintained by:</h1>
             <h1 className="text-lg text-white font-black text-center pt-1">SSMS Tech Team</h1>
-            <Link href={"https://docs.google.com/spreadsheets/d/1a970cJUm-ZE9lLSfMtfpF1xyohaWshjJ/edit?usp=sharing&ouid=106094898154491564206&rtpof=true&sd=true"} target="_blank"><h1 className="text-center text-textgreen pt-2 text-sm font-bold cursor-pointer">Download the menu</h1></Link>
+            <a href="../Menu/MENU 1st to 15th Oct-23.pdf" download={"Mess Menu"}><h1 className="text-center text-textgreen pt-2 text-sm font-bold cursor-pointer">Download the menu</h1></a>
         </main>
     );
 }
