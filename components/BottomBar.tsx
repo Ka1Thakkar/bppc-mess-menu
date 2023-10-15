e5a971adb0e4a1084456a0d59aff67a679c20feb
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2023-10-16 02:09:30 +0530

    Update Menu

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -5,7 +5,7 @@ const BottomBar = () => {
         <main className="absolute bottom-20 lg:bottom-5 w-full">
             <h1 className="text-base text-white/60 text-center">Developed and maintained by:</h1>
             <h1 className="text-lg text-white font-black text-center pt-1">SSMS Tech Team</h1>
-            <a href="../Menu/MENU 1st to 15th Oct-23.pdf" download={"Mess Menu"}><h1 className="text-center text-textgreen pt-2 text-sm font-bold cursor-pointer">Download the menu</h1></a>
+            <a href="../Menu/menu 16th to 31st oct-23-2.pdf" download={"Mess Menu"}><h1 className="text-center text-textgreen pt-2 text-sm font-bold cursor-pointer">Download the menu</h1></a>
         </main>
     );
 }
