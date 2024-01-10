92f6081a9f43fca0da080c15e4586d2e18953f79
Kavan Thakkar <95115101+Ka1Thakkar@users.noreply.github.com>
2024-01-10 19:50:49 +0530

    updated menu.js

--- components/day-menu.jsx
+++ components/day-menu.jsx
@@ -38,22 +38,22 @@ const DayMenu = ({index}) => {
         <h1 className='lg:text-5xl text-4xl pb-5 font-extrabold text-textgreen'>Breakfast</h1>
           <div className=''>
             {breakfast_consts.map((breakfast, i)=>(
-              <h1 key={i} className='text-center text-base lg:text-xl'>{breakfast}</h1>
+              <h1 key={i} className='text-center text-base lg:text-xl capitalize'>{breakfast}</h1>
             ))}
           </div>
-          <div className='flex flex-col gap-2 text-base lg:text-xl font-medium text-center'>{data[index].B.map((menuitem, key) => (
+          <div className='flex flex-col gap-2 text-base lg:text-xl font-medium text-center capitalize'>{data[index].B.map((menuitem, key) => (
             <h1 key={key}>{menuitem}</h1>
           ))}</div>
         </div>
         <div className=' flex flex-col justify-start items-center p-10 border-4 border-textgreen/50 text-white rounded-xl'>
           <h1 className='text-4xl lg:text-5xl pb-5 font-extrabold text-textgreen'>Lunch</h1>
-          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center'>{data[index].L.map((menuitem, key) => (
+          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center capitalize'>{data[index].L.map((menuitem, key) => (
             <h1 key={key}>{menuitem}</h1>
           ))}</div>
         </div>
         <div className=' flex flex-col justify-start items-center p-10 border-4 border-textgreen/50 text-white rounded-xl'>
           <h1 className='text-4xl md:text-5xl pb-5 font-extrabold text-textgreen'>Dinner</h1>
-          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center'>{data[index].D.map((menuitem, key) => (
+          <div className='flex flex-col gap-2 text-base md:text-xl font-medium text-center capitalize'>{data[index].D.map((menuitem, key) => (
             <h1 key={key}>{menuitem}</h1>
           ))}</div>
         </div>
