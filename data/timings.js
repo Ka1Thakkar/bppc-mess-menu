9df5799f49825b0799a873407da3a1cab53c4eff
Ka1Thakkar <f20211540@pilani.bits-pilani.ac.in>
2024-11-24 13:30:23 +0530

    winter timing updated

--- data/timings.js
+++ data/timings.js
@@ -8,7 +8,7 @@ const data = [{
     duration : "Winter",
     B: ["07:30 AM - 09:30 AM", "08:00 AM - 10:00 AM (Sundays and Holidays)"],
     L: ["11:30 AM - 01:30 PM", "12:00 PM - 02:00 PM (Sundays and Holidays)"],
-    D: ["06:30 PM - 08:15 PM", "(08:15 PM Last Entry Time)"]
+    D: ["06:30 PM - 08:00 PM", "(08:00 PM Last Entry Time)"]
 },
 ]
 
